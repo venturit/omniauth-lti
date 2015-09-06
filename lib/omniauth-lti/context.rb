508b5f78346149c73027d74ffc1fc39efcd8291b
Shekhar <shekhar@venturit.com>
2015-09-06 15:52:57 +0530

    Update context.rb

--- lib/omniauth-lti/context.rb
+++ lib/omniauth-lti/context.rb
@@ -32,6 +32,7 @@ module Omniauth
         return nil unless lti_launch_params
         key = lti_launch_params['oauth_consumer_key']
         secret = self.lti_credentials[key] if self.lti_credentials
+        puts lti_launch_params
         @tp ||= IMS::LTI::ToolProvider.new(key, 
                                            secret, 
                                            lti_launch_params)
